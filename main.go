0080414af17edccedf627afb6243ae23158e2657
John Sahhar <john@entropy.xyz>
2023-04-09 14:17:33 -0400

    add reading last log-file

--- main.go
+++ main.go
@@ -7,12 +7,35 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
+	"path"
+	"sort"
+	"strconv"
+)
+
+const (
+	LOG_DIR = "/etc/openai/logs"
 )
 
 var (
-	logger = SetupLogger("/etc/openai/logs")
+	logger = SetupLogger(LOG_DIR)
 )
+
+func sort_numerically(files []os.FileInfo) []os.FileInfo {
+	sort.Slice(files, func(i, j int) bool {
+		num1, err1 := strconv.Atoi(files[i].Name())
+		num2, err2 := strconv.Atoi(files[j].Name())
+
+		if err1 != nil || err2 != nil {
+			return files[i].Name() < files[j].Name()
+		}
+
+		return num1 < num2
+	})
+
+	return files
+}
 
 func main() {
 	client := args.parse()
@@ -25,6 +48,28 @@ func main() {
 		if _, err := client.available_models(); err != nil {
 			panic(err)
 		}
+		os.Exit(0)
+	}
+
+	if client.args.latest_logs {
+		files, err := ioutil.ReadDir(LOG_DIR)
+		if err != nil {
+			panic(err)
+			os.Exit(1)
+		}
+
+		files = sort_numerically(files)
+		if len(files) < 1 {
+			os.Stdout.WriteString(fmt.Sprintf("No log files found at %s\n", LOG_DIR))
+			os.Exit(0)
+		}
+		last := files[len(files)-2].Name()
+		os.Stdout.WriteString(fmt.Sprintf("reading log-file %s\n", last))
+		logf, err := os.Open(path.Join(LOG_DIR, last))
+		if err != nil {
+			panic(err)
+		}
+		io.Copy(os.Stdout, logf)
 		os.Exit(0)
 	}
 
